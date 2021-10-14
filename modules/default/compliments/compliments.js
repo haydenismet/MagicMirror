ddfd5ae009c4a74e08c3727ba513db066bdbb63e
joshua-tree-ctrl <55064706+joshua-tree-ctrl@users.noreply.github.com>
2021-10-14 22:07:45 +0100

    product hunt module development - styles and config

--- modules/default/compliments/compliments.js
+++ modules/default/compliments/compliments.js
@@ -8,10 +8,10 @@ Module.register("compliments", {
 	// Module config defaults.
 	defaults: {
 		compliments: {
-			anytime: ["Hey there sexy!"],
-			morning: ["Good morning, handsome!", "Enjoy your day!", "How was your sleep?"],
-			afternoon: ["Hello, beauty!", "You look sexy!", "Looking good today!"],
-			evening: ["Wow, you look hot!", "You look nice!", "Hi, sexy!"],
+			anytime: ["Hi Hayden"],
+			morning: ["Good morning!", "Enjoy your day!", "How was your sleep?"],
+			afternoon: ["Good afternoon", "Lunch?", "Breathe"],
+			evening: ["Wind down", "Relax", "Good Evening"],
 			"....-01-01": ["Happy new year!"]
 		},
 		updateInterval: 30000,
